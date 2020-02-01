382af2c127e462c0f81467e71b5139d62c9ed176
TestAlexey <KAA888@gmail.com>
2020-02-01 17:14:42 +0500

    test commited

---
+++ DjangoWebProject3/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Work\PythonDjango\Projects\GitTest\Test1\DjangoWebProject3\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
